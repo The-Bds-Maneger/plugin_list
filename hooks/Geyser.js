90a5ada5f286a5f89aacadb36a6a2099b9ebc1ce
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-09-23 20:50:14 -0300

    Update Geyser.js

--- hooks/Geyser.js
+++ hooks/Geyser.js
@@ -4,7 +4,7 @@ module.exports.scriptName = "Geyser";
 module.exports.register = function (actions) {
   actions.on("data", line => {
     if (portListen.test(line)) {
-      const geyserPort = line.match(geyserPortListen);
+      const geyserPort = line.match(portListen);
       let [,, host, port] = geyserPort;
       if (host === "*"||!host) host = "127.0.0.1";
       return actions.emit("portListening", {
